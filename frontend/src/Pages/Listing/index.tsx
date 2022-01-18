5f6f0a9f05907c283ee4b5b63643fe6d2faa928b
Igor Terriaga <igsantos1996@gmail.com>
2022-01-17 21:17:00 -0300

    Navigation

--- frontend/src/Pages/Listing/index.tsx
+++ frontend/src/Pages/Listing/index.tsx
@@ -1,12 +1,27 @@
-import MovieStars from "Components/MovieStars";
+import MovieCard from "Components/MovieCard";
 import Pagination from "Components/Pagination";
 
 function Listing() {
-  return(
-  <>
-    <Pagination />
-    <MovieStars />
-  </>
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-6  col-lg-4 col-xl-3 mb-3">
+            <MovieCard  />
+          </div>
+          <div className="col-sm-6  col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6  col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col-sm-6  col-lg-4 col-xl-3 mb-3">
+            <MovieCard />
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 export default Listing;
